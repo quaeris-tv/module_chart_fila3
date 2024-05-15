23b41ecd3e06bcf1d94d1f926b47b9cba86d37f2
marco76tv <marco.sottana@gmail.com>
2024-05-15 11:38:02 +0200

    up

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 // //use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
-use Modules\Xot\Services\FactoryService;
+use Modules\Xot\Actions\Factory\GetFactoryAction;
 use Modules\Xot\Traits\Updater;
 
 /**
@@ -60,6 +60,6 @@ abstract class BaseModel extends Model
      */
     protected static function newFactory(): Factory
     {
-        return FactoryService::newFactory(static::class);
+        return app(GetFactoryAction::class)->execute(static::class);
     }
 }
