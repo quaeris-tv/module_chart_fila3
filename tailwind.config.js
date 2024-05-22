d80774e7d3dfd4bf9cdc21c9b5054348d77ff5c1
marco76tv <marco.sottana@gmail.com>
2024-05-22 13:44:30 +0200

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     '../../resources/views/filament/**/*.blade.php',
     '../../vendor/filament/**/*.blade.php',
     '../../vendor/awcodes/overlook/resources/**/*.blade.php',
-    '../../Modules/**/*.php',
+    '../../Modules/Resources/**/*.php',
+    '../../Modules/Http/**/*.php',
+    '../../Modules/Filament/**/*.php',
+    '../../Modules/View/**/*.php',
   ],
   theme: {
     extend: {},
