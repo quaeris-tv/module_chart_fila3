fed7621e79fb6fb15e6c1a2f762aaa37f1bb31ef
marco <marco.sottana@gmail.com>
2024-08-26 10:25:01 +0200

    up

--- Models/MixedChart.php
+++ Models/MixedChart.php
@@ -12,18 +12,17 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\Relation;
-use Modules\Quaeris\Models\QuestionChart;
 
 /**
  * Modules\Chart\Models\MixedChart.
  *
  * @property Collection<int, \Modules\Chart\Models\Chart> $charts
- * @property int|null $charts_count
+ * @property int|null                                     $charts_count
  *
  * @method static \Modules\Chart\Database\Factories\MixedChartFactory factory($count = null, $state = [])
- * @method static Builder|MixedChart newModelQuery()
- * @method static Builder|MixedChart newQuery()
- * @method static Builder|MixedChart query()
+ * @method static Builder|MixedChart                                  newModelQuery()
+ * @method static Builder|MixedChart                                  newQuery()
+ * @method static Builder|MixedChart                                  query()
  *
  * @mixin \Eloquent
  */
@@ -40,7 +39,7 @@ class MixedChart extends BaseModel
     public function charts(): MorphMany
     {
         Relation::morphMap([
-            'question_chart' => QuestionChart::class,
+            'question_chart' => 'Modules\Quaeris\Models\QuestionChart',
             'mixed_chart' => self::class,
         ]);
 
